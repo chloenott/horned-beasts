af3634ba917268d78992e174a929266088eb159b
Chloe Nott <chloe@nott.email>
2021-10-25 18:05:04 -0700

    Completed lab-01, new components are added, stretch goal completed

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
