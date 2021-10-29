1fc34511d9f8c2d0ba1ac80886bfec28faaae22e
Chloe Nott <chloe@nott.email>
2021-10-28 19:37:54 -0700

    Refactored. Form now on Main component. Filter options now adjusts to json file contents.

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import Main from './Main.js'
 import Footer from './Footer.js'
 import SelectedBeast from './SelectedBeast.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Form, FloatingLabel } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import beasts from '../assets/data.json'
 
 export default class App extends React.Component {
@@ -24,26 +24,10 @@ export default class App extends React.Component {
     this.setState({showBeast: null});
   }
 
-  filterBeastsByHorn = (event) => {
-    let filterTo = event.target.value;
-    this.setState({
-      beasts: beasts.filter( beast => 
-        isNaN(filterTo) ? true : beast.horns === Number(filterTo) 
-      )
-    });
-  }
-
   render() {
     return (
       <Container>
         <Header />
-        <FloatingLabel label="Filter by number of horns">
-            <Form.Control as="select" aria-label="Label" onChange={this.filterBeastsByHorn}>
-              {['All', '1', '2', '3'].map( filterTo =>
-                  <option key={filterTo} value={filterTo}>{filterTo}</option>
-              )}
-            </Form.Control>
-          </FloatingLabel>
         <Main beasts={this.state.beasts} showDetails={this.showDetails}/>
         <Footer />
         <SelectedBeast beast={this.state.showBeast} handleHide={this.handleHide} />
